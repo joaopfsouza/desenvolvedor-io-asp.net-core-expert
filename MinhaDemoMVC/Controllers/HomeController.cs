8ff9f4f5f1f0484917b06b7a878258b08cd9c75d
João Paulo Ferreira de Souza <joao.psouza@kroton.com.br>
2020-03-18 15:24:36 -0300

    Personalização rotas

--- MinhaDemoMVC/Controllers/HomeController.cs
+++ MinhaDemoMVC/Controllers/HomeController.cs
@@ -9,6 +9,8 @@ using MinhaDemoMVC.Models;
 
 namespace MinhaDemoMVC.Controllers
 {
+    [Route("")]
+    [Route("gestao-clientes")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
@@ -18,17 +20,23 @@ namespace MinhaDemoMVC.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index(string id, string categorias)
+        [Route("")]
+        [Route("pagina-inicial")]
+        [Route("pagina-inicial/{id:int}/{categorias:guid?}")]
+        public IActionResult Index(int id, Guid categorias)
         {
             return View();
         }
 
+        [Route("privacidade")]
+        [Route("politica-privacidade")]
         public IActionResult Privacy()
         {
             return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        [Route("erro-encontrado")]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
