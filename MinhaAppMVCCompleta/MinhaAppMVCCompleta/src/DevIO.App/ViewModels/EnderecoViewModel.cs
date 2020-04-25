501c7f8710db6713705e1119cd56442bb3af3a8b
João Paulo Ferreira de Souza <joao.psouza@kroton.com.br>
2020-04-25 16:55:51 -0300

    Modal e Ajax de busca de CEP

--- MinhaAppMVCCompleta/MinhaAppMVCCompleta/src/DevIO.App/ViewModels/EnderecoViewModel.cs
+++ MinhaAppMVCCompleta/MinhaAppMVCCompleta/src/DevIO.App/ViewModels/EnderecoViewModel.cs
@@ -1,9 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DevIO.App.ViewModels
 {
@@ -13,33 +10,32 @@ namespace DevIO.App.ViewModels
         public Guid Id { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
-        [StringLength(200, ErrorMessage = "O campo {0} precisa ter entre {2} e {1}  ", MinimumLength = 2)]
+        [StringLength(200, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
         public string Logradouro { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
-        [StringLength(50, ErrorMessage = "O campo {0} precisa ter entre {2} e {1}  ", MinimumLength = 2)]
+        [StringLength(50, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 1)]
         public string Numero { get; set; }
 
-
         public string Complemento { get; set; }
-        public string Cep { get; set; }
-
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
-        [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1}  ", MinimumLength = 8)]
-
+        [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
         public string Bairro { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
-        [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1}  ", MinimumLength = 2)]
+        [StringLength(8, ErrorMessage = "O campo {0} precisa ter {1} caracteres", MinimumLength = 8)]
+        public string Cep { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
         public string Cidade { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
-        [StringLength(50, ErrorMessage = "O campo {0} precisa ter entre {2} e {1}  ", MinimumLength = 2)]
+        [StringLength(50, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
         public string Estado { get; set; }
 
         [HiddenInput]
         public Guid FornecedorId { get; set; }
-     
     }
 }
