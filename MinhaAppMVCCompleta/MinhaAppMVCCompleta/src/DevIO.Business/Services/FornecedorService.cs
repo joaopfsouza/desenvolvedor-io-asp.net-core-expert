90bacddd21c05b1d83807df5b94d2240a2bfead7
João Paulo Ferreira de Souza <joao.psouza@kroton.com.br>
2020-05-03 15:07:49 -0300

    Validações

---
+++ MinhaAppMVCCompleta/MinhaAppMVCCompleta/src/DevIO.Business/Services/FornecedorService.cs
@@ -0,0 +1,40 @@
+﻿using DevIO.Business.Interfaces;
+using DevIO.Business.Models;
+using DevIO.Business.Models.Validations;
+using System;
+using System.Threading.Tasks;
+
+namespace DevIO.Business.Services
+{
+    public abstract class FornecedorService : BaseService, IFornecedorService
+    {
+        public async Task Adicionar(Fornecedor fornecedor)
+        {
+            // Validar o estado da entidade!
+            if (!ExecutarValidacao(new FornecedorValidation(), fornecedor)
+                && !ExecutarValidacao(new EnderecoValidation(), fornecedor.Endereco)) return;
+
+           
+
+            // Se não existe fornecedor com o mesmo documento
+
+
+        }
+
+        public async Task Atualizar(Fornecedor fornecedor)
+        {
+            if (!ExecutarValidacao(new FornecedorValidation(), fornecedor))
+              return;
+        }
+
+        public async Task AtualizarEndereco(Endereco endereco)
+        {
+            if (!ExecutarValidacao(new EnderecoValidation(), endereco)) return;
+        }
+
+        public Task Remover(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MinhaAppMVCCompleta/MinhaAppMVCCompleta/src/DevIO.Business/Services/ProdutoService.cs
@@ -0,0 +1,26 @@
+﻿using DevIO.Business.Interfaces;
+using DevIO.Business.Models;
+using DevIO.Business.Models.Validations;
+using System;
+using System.Threading.Tasks;
+
+namespace DevIO.Business.Services
+{
+    public abstract class ProdutoService : BaseService, IProdutoService
+    {
+        public async Task Adicionar(Produto produto)
+        {
+            if (!ExecutarValidacao(new ProdutoValidation(), produto)) return;
+        }
+
+        public async Task Atualizar(Produto produto)
+        {
+            if (!ExecutarValidacao(new ProdutoValidation(), produto)) return;
+        }
+
+        public Task Remover(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
