e6352cb2322e0ce74c9febee4be4c16b823c7c75
João Paulo Ferreira de Souza <joao.psouza@kroton.com.br>
2020-05-24 18:38:07 -0300

    Api simples

---
+++ MinhaApiCoreVisual/MinhaApiCoreVisual/Controllers/ValuesController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MinhaApiCoreVisual.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            var valores = new string[] { "value1", "value2" };
+
+            if (valores.Length == 5000)
+                return BadRequest();
+
+            return Ok(valores);
+        }
+
+
+        [HttpGet("obter-resultados")]
+        public ActionResult ObterResultado()
+        {
+            var valores = new string[] { "value1", "value2" };
+
+            if (valores.Length < 5000)
+                return BadRequest();
+
+            return Ok(valores);
+        }
+
+        [HttpGet("obter-valores")]
+        public IEnumerable<string> ObterValores()
+        {
+            var valores = new string[] { "value1", "value2" };
+
+            if (valores.Length < 5000)
+                return null;
+
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("obter-id/{id:int}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        [ProducesResponseType(typeof(Product), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult Post(Product product)
+        {
+
+            if (product.Id == 0) return BadRequest();
+
+           // return Ok(product);
+            return CreatedAtAction(nameof(Post), product);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+
+    public class Product
+    {
+        public int Id { get; internal set; }
+    }
+}
